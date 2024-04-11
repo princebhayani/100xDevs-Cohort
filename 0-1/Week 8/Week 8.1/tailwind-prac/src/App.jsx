084fdc22ac71027bf0773b3e5302c81f160404a3
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-11 22:17:07 +0530

    week 8.1

---
+++ 0-1/Week 8/Week 8.1/tailwind-prac/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='grid md:grid-cols-3'>
+        <div className="bg-red-400 md:bg-yellow-400">hi</div>
+        <div className="bg-blue-600 md:bg-yellow-500">hi</div>
+        <div className="bg-green-600 md:bg-yellow-600">hi</div>
+        <div className="bg-slate-600 md:bg-yellow-700">hi</div>
+        <div className="bg-red-950 md:bg-yellow-800">hi</div>
+        <div className="bg-yellow-600 md:bg-yellow-900">hi</div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
+  
