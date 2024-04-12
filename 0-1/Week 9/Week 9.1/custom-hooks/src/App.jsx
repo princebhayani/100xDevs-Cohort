c695c323fad768587757cae2fe6591f9955943f7
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-12 23:14:22 +0530

    week 9.1

---
+++ 0-1/Week 9/Week 9.1/custom-hooks/src/App.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function useTodos(n) {
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+        .then(res => {
+          setTodos(res.data.todos);
+          setLoading(false);
+        })
+    }, n * 1000)
+  
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      })
+
+    return () => {
+      clearInterval(value)
+    }
+  }, [n])
+
+  return {todos, loading};
+}
+
+function App() {
+  const {todos, loading} = useTodos(10);
+
+  if (loading) {
+    return <div> loading... </div>
+  }
+
+  return (
+    <>
+      {todos.map(todo => <Track todo={todo} />)}
+    </>
+  )
+}
+
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
+}
+
+export default App
