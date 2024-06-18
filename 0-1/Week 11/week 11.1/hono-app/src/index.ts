fe8e3d374e1f2974006d7cc9a1da498b63365b75
princebhayani1902 <princebhayani192003@gmail.com>
2024-06-18 15:43:16 +0530

    week 11.1

---
+++ 0-1/Week 11/week 11.1/hono-app/src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.text('Hello Hono!')
+})
+
+export default app
